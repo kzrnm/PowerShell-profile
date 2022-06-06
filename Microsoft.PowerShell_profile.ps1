84b7b919c67b1e78887845747c0d98b8e10c86e9
kzrnm <gengesa@gmail.com>
2022-06-06 21:30:46 +0900

    posh-gitを二重読み込みしていた

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -1,8 +1,7 @@
 $env:LANG = "ja_JP.UTF-8"
+. "$PSScriptRoot/Scripts/local_alias.ps1"
 . "$PSScriptRoot/Scripts/completion.ps1"
 . "$PSScriptRoot/Scripts/functions.ps1"
-. "$PSScriptRoot/Scripts/local_alias.ps1"
-
 
 Set-Alias sleep Start-SleepKurukuru -Force
 Set-Alias ll Get-ChildItem
@@ -10,14 +9,14 @@ Set-Alias clip Set-Clipboard
 function which { (Get-Command $args).Definition }
 function bd { Set-Location .. }
 
-
 $env:POSH_GIT_ENABLED = $True
+
 if ($env:NO_NERD_FONT) {
     Set-PoshPrompt hotstick.minimal
     Remove-Module oh-my-posh
 }
 elseif ($env:TERMINAL_EMULATOR -eq "JetBrains-JediTerm") {
-#    $GitPromptSettings.EnableFileStatus = $false
+    #    $GitPromptSettings.EnableFileStatus = $false
     $GitPromptSettings.BranchGoneStatusSymbol.Text = '*'
     $GitPromptSettings.BranchIdenticalStatusSymbol.Text = '_'
     $GitPromptSettings.BranchAheadStatusSymbol.Text = 'L'
--- Scripts/completion.ps1
+++ Scripts/completion.ps1
@@ -1,11 +1,11 @@
 Register-ArgumentCompleter -Native -CommandName winget -ScriptBlock {
     param($wordToComplete, $commandAst, $cursorPosition)
-        [Console]::InputEncoding = [Console]::OutputEncoding = $OutputEncoding = [System.Text.Utf8Encoding]::new()
-        $Local:word = $wordToComplete.Replace('"', '""')
-        $Local:ast = $commandAst.ToString().Replace('"', '""')
-        winget complete --word="$Local:word" --commandline "$Local:ast" --position $cursorPosition | ForEach-Object {
-            [System.Management.Automation.CompletionResult]::new($_, $_, 'ParameterValue', $_)
-        }
+    [Console]::InputEncoding = [Console]::OutputEncoding = $OutputEncoding = [System.Text.Utf8Encoding]::new()
+    $Local:word = $wordToComplete.Replace('"', '""')
+    $Local:ast = $commandAst.ToString().Replace('"', '""')
+    winget complete --word="$Local:word" --commandline "$Local:ast" --position $cursorPosition | ForEach-Object {
+        [System.Management.Automation.CompletionResult]::new($_, $_, 'ParameterValue', $_)
+    }
 }
 
 Register-ArgumentCompleter -Native -CommandName dotnet -ScriptBlock {
@@ -17,4 +17,3 @@ Register-ArgumentCompleter -Native -CommandName dotnet -ScriptBlock {
 . C:\Software\ripgrep\_rg.ps1
 
 Import-Module npm-completion
-Import-Module posh-git
